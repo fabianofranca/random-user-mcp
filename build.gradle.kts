4d72e272230fb8b6f6c225b2131e8526e63896d6
Fabiano França <email@fabianofranca.com>
2025-07-15 15:41:01 -0300

    feat: Integrate Jacoco for code coverage reporting
    Added Jacoco plugin to monitor code coverage. Configured reports in XML and HTML formats, set up exclusions for specific directories, and linked test tasks to finalize with Jacoco report generation.

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     alias(libs.plugins.kotlin.jvm)
     alias(libs.plugins.kotlinx.serialization)
     alias(libs.plugins.ktor)
+    alias(libs.plugins.jacoco)
 }
 
 group = "com.fabianofranca"
@@ -31,4 +32,31 @@ dependencies {
     testImplementation(libs.ktor.server.test.host)
     testImplementation(libs.kotlin.test.junit)
     testImplementation(libs.ktor.client.mock)
+}
+
+jacoco {
+    toolVersion = "0.8.11"
+}
+
+tasks.jacocoTestReport {
+    reports {
+        xml.required.set(true)
+        html.required.set(true)
+        csv.required.set(false)
+    }
+
+    classDirectories.setFrom(
+        files(classDirectories.files.map {
+            fileTree(it) {
+                exclude(
+                    "**/models/**",
+                    "**/Application*"
+                )
+            }
+        })
+    )
+}
+
+tasks.test {
+    finalizedBy(tasks.jacocoTestReport)
 }
