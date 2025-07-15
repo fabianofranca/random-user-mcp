cf2d5b63b8ab617928a4308c98f21c5668802309
Fabiano França <email@fabianofranca.com>
2025-07-15 17:07:47 -0300

    docs: Expand nationality parameter description with version-specific options
    Added detailed documentation for the `nationality` parameter, outlining supported nationalities by API version from 1.0 to 1.4 in `GetUsersTool`.

--- src/main/kotlin/com/fabianofranca/randomuser/tools/GetUsersTool.kt
+++ src/main/kotlin/com/fabianofranca/randomuser/tools/GetUsersTool.kt
@@ -31,7 +31,12 @@ class GetUsersTool(
                     default(GetUsersArgs.DEFAULT_PAGE)
                 }
                 string(GetUsersArgs.PARAM_NATIONALITY) {
-                    description("Nationality of users to be returned (default: ${GetUsersArgs.DEFAULT_NATIONALITY}).")
+                    description("Nationality of users to be returned (default: ${GetUsersArgs.DEFAULT_NATIONALITY}). Available options by version: " +
+                            "1.0: AU (Australia), BR (Brazil), CA (Canada), CH (Switzerland), DE (Germany), DK (Denmark), ES (Spain), FI (Finland), FR (France), GB (United Kingdom), IE (Ireland), IR (Iran), NL (Netherlands), NZ (New Zealand), TR (Turkey), US (United States); " +
+                            "1.1: Added NO (Norway); " +
+                            "1.2: Added IN (India); " +
+                            "1.3: Added MX (Mexico), RS (Serbia); " +
+                            "1.4: Added UA (Ukraine).")
                     default(GetUsersArgs.DEFAULT_NATIONALITY)
                 }
                 string(GetUsersArgs.PARAM_VERSION) {
